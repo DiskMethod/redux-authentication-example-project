abdbbe3c958ad78d26edcc13a32f6cfbe0dce1df
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-07-23 17:12:21 -0400

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
+import RequireAuth from "./features/auth/RequireAuth";
 
 const App = () => {
-  return;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route element={<RequireAuth />}>
+          <Route path="welcome" element={<Welcome />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -11,3 +11,5 @@ export const authApi = apiSlice.injectEndpoints({
     }),
   }),
 });
+
+export const { useLoginMutation } = authApi;
